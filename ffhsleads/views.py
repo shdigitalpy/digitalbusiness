d9e53d14bbb6d5f3ed81456a77d607c9a512c07a
Sandro Huber <sandro@sh-digital.ch>
2020-11-13 16:05:23 +0100

    first-draft layout

--- ffhsleads/views.py
+++ ffhsleads/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from datetime import datetime
+
 
 def index(request):
-	context = {}
+	x = datetime.now()
+	today = x.strftime('%m' + '.' + '%Y')
+	now = x.strftime('%d' + '.' + '%m' + '.' + '%Y' + ' ' + '%H' + ':' + '%M')
+
+	import random
+	zufall = random.randint(5, 19)
+
+	context = {'today': today, 'zufall': zufall, 'now': now }
 	return render(request, 'index.html', context)
