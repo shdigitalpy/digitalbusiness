1fa1a6245338239f50f091f4d3cbeef59175a972
Sandro Huber <sandro@sh-digital.ch>
2020-11-17 12:13:29 +0100

    email send

--- ffhsleads/views.py
+++ ffhsleads/views.py
@@ -10,15 +10,15 @@ from django.utils.html import strip_tags
 def index(request):
 
 	if request.method == "POST":
-		message_name = request.POST['message_name']
-		message_vorname = request.POST['message_vorname']
-		message_email = request.POST['message_email']
-		message_telefon = request.POST['message_phone']
-		message_bemerkungen = request.POST['message_bemerkungen']
+		message_name = request.POST['name']
+		message_vorname = request.POST['vorname']
+		message_email = request.POST['email']
+		message_telefon = request.POST['phone']
+		message_bemerkungen = request.POST['bemerkungen']
 
 		send_mail(
 			'FFHS Anfrage - ' + message_name + message_vorname,
-			message_name+ ' <br>' + message_vorname + ' <br>' + message_email + ' <br>' + message_telefon + ' <br>' + message_bemerkungen,
+			'Name: '+ message_name + ' Vorname: ' + message_vorname + ' E-Mail: ' + message_email + ' Telefon: ' + message_telefon + ' Bemerkungen: ' + message_bemerkungen,
 			message_email,
 			['sandro@sh-digital.ch'],
     		fail_silently=False,		
@@ -34,5 +34,5 @@ def index(request):
 		import random
 		zufall = random.randint(5, 19)
 
-		context = {'today': today, 'zufall': zufall, 'now': now }
+		context = {'today': today, 'zufall': zufall, 'now': now}
 		return render(request, 'index.html', context)
