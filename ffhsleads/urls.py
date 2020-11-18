ac6032ea203296b786b7217cfd052cb9a0a0df01
Sandro Huber <sandro@sh-digital.ch>
2020-11-18 15:22:58 +0100

    robots.txt

--- ffhsleads/urls.py
+++ ffhsleads/urls.py
@@ -2,6 +2,8 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 from .sitemaps import StaticViewSitemap
 from . import views
+from django.views.generic.base import TemplateView
+
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -12,6 +14,10 @@ sitemaps = {
 urlpatterns = [
     path('', views.index, name="index"),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
 
